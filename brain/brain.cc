825b5509041c291b2ce9e299ea0ffa266b9a8cd5
Nolan <bocknolan@gmail.com>
2020-10-07 14:38:13 -0400

    updated state machine

--- brain/brain.cc
+++ brain/brain.cc
@@ -11,6 +11,8 @@ using std::endl;
 int file = 0;
 bool wallFound = false;
 bool facingDoor = false;
+bool transition = false;
+bool backWall = false;
 
 // we know the goal state is always on the 11th file
 int goalFile = 10;
@@ -19,28 +21,48 @@ void
 callback(Robot* robot)
 {
     
-    // TODO: fix file system, not working at all
-    // TODO: fix going through passages that are facing the wrong way, never an incentive for us unless on 12th file
-    // TODO: might be better to not go straight through doors, but try to find the next wall (this could be our file tracking)
-    
+    // Celebrate: backwall and file tracker are WORKIGN
+    // TODO: I shouldn't turn or increment file when on a backwall - this is causing loops
+    // TODO: goal file has no logic, gonna leave that for last as that should be relatively simple.
+
     cout << "Range: " << robot->range << endl;
     cout << "Heading: " << robot->pos_t << endl;
     cout << "File: " << file << endl;
+    cout << "Wallfound: " << wallFound << endl;
+    cout << "Backwall: " << backWall << endl;
 
-    if (robot->pos_t < 0.02 && robot->range > 998 && !wallFound) {
-        robot->set_vel(5.0, 5.0);
-        return;
+    //if (robot->pos_t < 0.02 && robot->range > 998 && !wallFound) {
+      //  robot->set_vel(5.0, 5.0);
+        //return;
+    //}
+   
+
+    if (robot->range < 998) {
+        wallFound = true;
     }
-    
-    if (robot->range < 1.0) {
-       int heading = robot->pos_t;
 
-        if (heading < 0) {
+    
+    // if facing backwards, orient another direction
+    if (wallFound && (robot->pos_t < -1.7 || robot->pos_t > 1.7)) {
+        backWall = true;
+    } else if (wallFound) {
+        backWall = false;
+    }
+
+    if (robot->range < 1.0) {
+        int heading = robot->pos_t;
+
+        if (heading > 0 && heading < 0.75) {
+            // facing left
+            robot->set_vel(-5.0, 5.0);
+        } else if (heading < 0 && heading > -0.75) {
+            // facing right
             robot->set_vel(5.0, -5.0);
-        } else if (heading >= 0) {
+        } else {
             robot->set_vel(-5.0, 5.0);
         }
         wallFound = true;
+        transition = false;
         return;
  	}
 
@@ -53,30 +75,40 @@ callback(Robot* robot)
             robot->set_vel(-2.0, 5.0);
         }
 
+        transition = false;
+
         return;
     }
 
  
     // range is 999 when we have no sonar hits, but it seems to be rounded in output
-    if (robot->range > 998) {
-
-         
-        if (robot->pos_t < 0.1) {
-            cout << "Moving to new file" << endl;
-            file += 1;
-        }
+    if (robot->range > 998 && !transition) {
 
         wallFound = false;
         // keep track when we're along a wall (could be left or right)
-        robot->set_vel(5.0, 5.0);
+       
+        if (robot->pos_t > 0) { 
+            robot->set_vel(5.0, 0.0);
+        } else if (robot->pos_t <= 0) {
+            robot->set_vel(0.0, 5.0);
+        }
+        if (!transition && !backWall) {
+            cout << "Moving to new file" << endl;
+            file += 1;
+            transition = true;
+            
+        }
         return;
     }
 
+ 
+    
+    if (!transition) {
+        robot->set_vel(5.0, 5.0);
+    }
 
-
-    // move forwaard
-    robot->set_vel(5.0, 5.0);
 }
+
 
 int
 main(int argc, char* argv[])
